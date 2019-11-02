f936237bcf682c491f89364a49952100716f82ce
kazu <180441147@ccmailg.meijo-u.ac.jp>
2019-11-02 18:54:29 +0900

    json形式に直しました

--- background.js
+++ background.js
@@ -1,17 +1,44 @@
-chrome.contextMenus.create({title:"コンテキストメニューを追加",contexts:["selection"],onclick:change()});
+/*
+chrome.contextMenus.create({title:"コンテキストメニューを追加",contexts:["selection"],onclick:chang}function{
 
-function change(info, tab){
-  console.log(info, tab)
-  //<iframe src="https://yellow.gray-code.com/" width="600" height="400"　background="#000000"></iframe>
-  /*chrome.tabs.getSelected(null, function(tab) {
-            chrome.tabs.sendRequest(tab.id,{});*/
-  var selection = window.getSelection();
-  console.log("セレクション=" + selection);
+};*/
 
-  prompt(String(selection));
-  /*if(selection.getCount() > 0){
+chrome.contextMenus.create({title: "コンテキストメニュー登録", contexts:["selection"],onclick: change}, function(){
+     alert("コンテキストメニュー登録完了");
+});
 
-  }*/
+function change(info){
 
-
+/*
+  prompt(String(info.selectionText));
+  alert(String(info.pageUrl));
+  */
+sendData({question:info.selectionText,url:info.pageUrl});
 };
+
+function sendData(data) {
+  var XHR = new XMLHttpRequest();
+  var FD  = new FormData();
+  var name;
+
+  // データを FormData オブジェクトに投入します
+  for(name in data) {
+    FD.append(name, data[name]);
+  }
+
+  // データが正常に送信された場合に行うことを定義します
+  XHR.addEventListener('load', function(event) {
+    alert('Yeah! Data sent and response loaded.');
+  });
+
+  // エラーが発生した場合に行うことを定義します
+  XHR.addEventListener('error', function(event) {
+    alert('Oups! Something goes wrong.');
+  });
+
+  // リクエストをセットアップします
+  XHR.open('POST', 'http://ucommbieber.unl.edu/CORS/cors.ph');
+
+  // FormData オブジェクトを送信するだけです。HTTP ヘッダは自動的に設定されます
+  XHR.send(FD);
+}
