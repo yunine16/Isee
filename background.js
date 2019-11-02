6056d1d7ae3190753f125ec39428f9946aa88d79
kazu <180441147@ccmailg.meijo-u.ac.jp>
2019-11-02 17:45:00 +0900

    質問ページを表示

--- background.js
+++ background.js
@@ -1,6 +1,17 @@
-chrome.contextMenus.create({title:"コンテキストメニューを追加",contexts:["selection"],onclick:change},function(){
-  //alert("設定完了");
-});
+chrome.contextMenus.create({title:"コンテキストメニューを追加",contexts:["selection"],onclick:change()});
 
-function change(){
+function change(info, tab){
+  console.log(info, tab)
+  //<iframe src="https://yellow.gray-code.com/" width="600" height="400"　background="#000000"></iframe>
+  /*chrome.tabs.getSelected(null, function(tab) {
+            chrome.tabs.sendRequest(tab.id,{});*/
+  var selection = window.getSelection();
+  console.log("セレクション=" + selection);
+
+  prompt(String(selection));
+  /*if(selection.getCount() > 0){
+
+  }*/
+
+
 };
